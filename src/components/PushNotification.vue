37cf5668d7e0b042979c52f7d5b1fa32088c5c92
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-08-17 20:08:56 +0300

    create PushNotification component

---
+++ src/components/PushNotification.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "PushNotification",
+  props: {
+    iconSrc: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    time: {
+      type: String,
+      required: true
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    person: {
+      type: String,
+      default: ''
+    },
+  },
+})
+
+</script>
+
+<template>
+
+  <div class="push__box">
+    <div class="push__content">
+      <div class="push__top">
+        <div class="col">
+          <div class="push__imgwrap">
+            <img class="push__icon" :src="iconSrc" alt="">
+          </div>
+          <span class="push__title">{{ title }}</span>
+        </div>
+        <div class="col">
+          <div class="push__time">{{ time }}</div>
+        </div>
+      </div>
+      <div class="push__main">
+        <div v-if="person" class="push__person">{{ person }}</div>
+        <p>{{ message }}</p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
