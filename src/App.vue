de78adbb78aae716f263fbc3825ddf52aed260da
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-08-17 20:23:15 +0300

    add delayed push notifications for shorts

--- src/App.vue
+++ src/App.vue
@@ -16,20 +16,45 @@ onMounted(() => {
   pushesStore.addPush({
     id: "msg-dima",
     iconSrc: "https://raw.githubusercontent.com/WebDevPie/Create-iOS-Push-Notifications-Popup-HTML-CSS-and-JavaScript/master/img/icon-ios-msg.png",
-    title: "Messages",
-    time: "1d ago",
-    person: "John Smith",
-    message: "Are you listening?",
+    title: "Сообщения",
+    time: "13 часов назад",
+    person: "Димон (бар)",
+    message: "Слыш, когда касарь вернёшь? Уже месяц прошёл.",
   })
 
   pushesStore.addPush({
     id: "yt",
     iconSrc: "https://uploads-ssl.webflow.com/61fd00481f4b997da0ec34e3/62947eb4354f097a75cb3668_yt.png",
     title: "youtube",
-    time: "6h ago",
-    person: "LTT posted a new video",
-    message: "Watch video 'How many GPUs can fit into...'",
+    time: "9 часов назад",
+    person: "Новый подписчик!",
+    message: "На ваш канал уже подписалось целых 2378 чел...",
   })
+
+  setTimeout(() => {
+
+    pushesStore.addPush({
+      id: "t-1",
+      iconSrc: "https://acdn.tinkoff.ru/static/pages/files/ba002123-6b93-4af2-855e-3a33b8bc0a08.png",
+      title: "tinkoff",
+      time: "сейчас",
+      person: "Пополнение +390.000₽, счёт RUB",
+      message: "Доход, карта *0228. Баланс 391.337.42₽",
+    })
+
+    setTimeout(() => {
+
+      pushesStore.addPush({
+        id: "t-2",
+        iconSrc: "https://acdn.tinkoff.ru/static/pages/files/ba002123-6b93-4af2-855e-3a33b8bc0a08.png",
+        title: "tinkoff",
+        time: "сейчас",
+        person: "Пополнение +540.000₽, счёт RUB",
+        message: "Доход, карта *0228. Баланс 931.337.42₽",
+      })
+
+    }, 3000)
+  }, 4000)
 
 })
 </script>
